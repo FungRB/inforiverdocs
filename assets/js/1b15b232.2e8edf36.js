c546d66a0c02a8ceb73d342bd90a70aee4595417
divyabhushan <divya_bhushan@hotmail.com>
2021-05-13 13:52:38 +0200

    Deploy website - based on 956b921db06887f5601a8e859a7455881679e616

---
+++ assets/js/1b15b232.2e8edf36.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinforiver=self.webpackChunkinforiver||[]).push([[7169],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/inforiver/blog/welcome"},{"title":"Hello","permalink":"/inforiver/blog/hello-world"},{"title":"Hola","permalink":"/inforiver/blog/hola"}]}')}}]);
